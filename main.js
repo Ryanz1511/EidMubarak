20c92f86b8cf92ec0d4ef2c48c0f0e7ad49d9e60
Ryan <yan1511@gmail.com>
2026-03-20 17:54:50 +0700

    update

--- main.js
+++ main.js
@@ -166,15 +166,3 @@
 
 
 
-
-window.addEventListener('load', () => {
-  const audio = document.getElementById('bg-audio');
-
-  // set mulai dari detik 11
-  audio.currentTime = 11;
-
-  audio.muted = false;
-  audio.play().catch(() => {
-    console.log("Autoplay diblokir browser");
-  });
-});
